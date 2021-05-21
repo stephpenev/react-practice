5a2d00a21d896b448d93a07a0046adb82983cd46
stephp <stephanie.penev@gmail.com>
2021-05-21 14:40:24 -0400

    further practice on to do app

--- src/Components/ToDoItem.js
+++ src/Components/ToDoItem.js
@@ -2,9 +2,12 @@ import React from "react";
 
 function ToDoItem(props) {
   const { id, text, completed } = props;
+  const handleChange = () => {
+    console.log('changed');
+  }
   return (
     <div className="flexContainer" key={id}>
-      <input type="checkbox" name="check" checked={completed} />
+      <input type="checkbox" name="check" checked={completed} onChange={handleChange} />
       <label for="check"></label>
       <p>{text}</p>
     </div>
